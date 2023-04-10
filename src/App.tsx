9a0a303ce89ddbe3127928b0cf109cc13660fd42
Thiago Barcelos <thiagolbf@hotmail.com>
2023-04-10 06:56:50 -0300

    initial configurations

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { ButtonContainer } from "./components/Button/style";
+
+import { GlobalSTyle } from "./styles/globa";
+
+import { useState } from "react";
+
 export const App = () => {
-  return <h1>timer</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <ButtonContainer variant="primary"></ButtonContainer>
+
+      <GlobalSTyle />
+    </ThemeProvider>
+  );
 };
 
 export default App;
